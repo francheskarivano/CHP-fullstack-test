a13502cd3492c1f1a27bde01d920724766ef938e
Cheska <cheska.portfolio2018@gmail.com>
2020-10-28 15:34:22 +0800

    Initial Commit for CHP Full Stack Test

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form>
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username">
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="text" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" name="login" class="btn"></button>
+		</div>
+	</form>
+</body>
+</html>
